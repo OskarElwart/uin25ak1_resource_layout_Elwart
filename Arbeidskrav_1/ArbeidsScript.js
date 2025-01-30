26413c6e6d5e4612d0a35fd7bbd5698d760db95f
OskarElwart <112538308+OskarElwart@users.noreply.github.com>
2025-01-30 18:56:32 +0100

    dokument formatting

--- Arbeidskrav_1/ArbeidsScript.js
+++ Arbeidskrav_1/ArbeidsScript.js
@@ -1,20 +1,21 @@
 console.log(resources)
 let resoruceHTML = ""
 let linkPage = "HTML"
-document.getElementById("HTMLlink").addEventListener("click", function(){ResetClass(), UpdateLinkHTML(), UpdateMap()})
-document.getElementById("CSSlink").addEventListener("click", function(){ResetClass(), UpdateLinkCSS(), UpdateMap()})
-document.getElementById("JSlink").addEventListener("click", function(){ResetClass(), UpdateLinkJS(), UpdateMap()})
-document.getElementById("Reactlink").addEventListener("click", function(){ResetClass(), UpdateLinkReact(), UpdateMap()})
-document.getElementById("Sanitylink").addEventListener("click", function(){ResetClass(), UpdateLinkSanity(), UpdateMap()})
+document.getElementById("HTMLlink").addEventListener("click", function () { ResetClass(), UpdateLinkHTML(), UpdateMap() })
+document.getElementById("CSSlink").addEventListener("click", function () { ResetClass(), UpdateLinkCSS(), UpdateMap() })
+document.getElementById("JSlink").addEventListener("click", function () { ResetClass(), UpdateLinkJS(), UpdateMap() })
+document.getElementById("Reactlink").addEventListener("click", function () { ResetClass(), UpdateLinkReact(), UpdateMap() })
+document.getElementById("Sanitylink").addEventListener("click", function () { ResetClass(), UpdateLinkSanity(), UpdateMap() })
 const catge = resources.filter(
-    (element) => element.category  === linkPage
-  )
+    (element) => element.category === linkPage
+)
 //Brukte ChatGPT for linja 21, 22. Klarte ikke å finne en bra måte får å bruke en nested array. 
 //prompten var "how can i map() an array inside an array in javascript" med resources.map og arrayen pastet inn.
 //den lå til også target="_blank" for å åpne lenken i ny fane og .join(" ") for å ta bort komma fra html
 //som er ting jeg ikke ville ha funnet ut på egen hånd
-catge.map((resource, index) => { resoruceHTML += 
-            `<article>
+catge.map((resource, index) => {
+    resoruceHTML +=
+    `<article>
                 <h1>${resource.category}</h1>
                 <p>${resource.text}</p>
                 <ul>
@@ -22,34 +23,34 @@ catge.map((resource, index) => { resoruceHTML +=
                 <li><a href="${source.url}" target="_blank">${source.title}</a></li>`).join('')}
                 </ul>
             </article>`
-        })
-function ResetClass(){
-            document.getElementById("HTMLlink").className = "ButtonInactive"
-            document.getElementById("CSSlink").className = "ButtonInactive"
-            document.getElementById("JSlink").className = "ButtonInactive"
-            document.getElementById("Reactlink").className = "ButtonInactive"
-            document.getElementById("Sanitylink").className = "ButtonInactive"
+})
+function ResetClass() {
+    document.getElementById("HTMLlink").className = "ButtonInactive"
+    document.getElementById("CSSlink").className = "ButtonInactive"
+    document.getElementById("JSlink").className = "ButtonInactive"
+    document.getElementById("Reactlink").className = "ButtonInactive"
+    document.getElementById("Sanitylink").className = "ButtonInactive"
 }
-function UpdateLinkHTML(){
+function UpdateLinkHTML() {
     linkPage = "HTML"
     document.getElementById("HTMLlink").className = "ButtonActive"
 
 }
-function UpdateLinkCSS(){
+function UpdateLinkCSS() {
     linkPage = "CSS"
     document.getElementById("CSSlink").className = "ButtonActive"
 }
-function UpdateLinkJS(){
+function UpdateLinkJS() {
     linkPage = "JavaScript"
     document.getElementById("JSlink").className = "ButtonActive"
 
 }
-function UpdateLinkReact(){
+function UpdateLinkReact() {
     linkPage = "React"
     document.getElementById("Reactlink").className = "ButtonActive"
 
 }
-function UpdateLinkSanity(){
+function UpdateLinkSanity() {
     linkPage = "Sanity and headless CMS"
     document.getElementById("Sanitylink").className = "ButtonActive"
 
@@ -57,12 +58,13 @@ function UpdateLinkSanity(){
 
 
 
-function UpdateMap (){
+function UpdateMap() {
     resoruceHTML = ""
     const catge = resources.filter(
-        (element) => element.category  === linkPage
-      )
-    catge.map((resource, index) => { resoruceHTML += 
+        (element) => element.category === linkPage
+    )
+    catge.map((resource, index) => {
+        resoruceHTML +=
         `<article>
             <h1>${resource.category}</h1>
             <p>${resource.text}</p>
